970a0b9fd59a99bcf27247b7797df32ef95e4fba
Chingiz Mammadli <memmmedlicngz@gmail.com>
2016-08-23 16:53:34 +0400

    Header ready,animations and next/prev buttons work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
+// Header
+var i=1;
 
+$("#header .fa-chevron-right").on("click",function(){
+	$("#header #container"+i).fadeOut(400);
+	i++; checkI();
+	$("#header #container"+i).delay(400).fadeIn();
+	
+})
+
+$("#header .fa-chevron-left").on("click",function(){
+	$("#header #container"+i).fadeOut(400);
+	i--; checkI();
+	$("#header #container"+i).delay(400).fadeIn();
+})
+
+	// Function to keep i between 1 and 3
+	function checkI(){
+		if(i==4){
+			i=1;
+		}else if(i==0){
+			i=3;
+		}
+	}
