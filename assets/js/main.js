adf3e630c69348fd4fa718c3acfec374f774cfb5
Chingiz Mammadli <memmmedlicngz@gmail.com>
2016-08-24 20:38:27 +0400

    Offers section ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,14 @@ $("#header .fa-chevron-left").on("click",function(){
 			i=3;
 		}
 	}
+
+// Offers
+	// Function to make all divs' height same
+	var maxHeight=0;
+	for(i=1;i<5;i++){
+		thisH=$("#offers .col-md-3:nth-child("+i+")").height();
+		if(thisH>maxHeight){
+			maxHeight=thisH;
+		}
+	}
+	$("#offers .col-md-3 .offer").height(thisH);
