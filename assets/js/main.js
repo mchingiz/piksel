ce9aed54bf1afb6a167cd6ae8087a409c785bd96
Chingiz Mammadli <memmmedlicngz@gmail.com>
2016-08-25 14:56:23 +0400

    Added floating element animations to #container1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,37 @@
 // Header
-var sliderNum=1;
+	// Slider
+	var sliderNum=1;
+	$("#header .fa-chevron-right").on("click",function(){
+		$("#header #container"+sliderNum).fadeOut(400);
+		sliderNum++; checkI();
+		$("#header #container"+sliderNum).delay(400).fadeIn();
 
-$("#header .fa-chevron-right").on("click",function(){
-	$("#header #container"+sliderNum).fadeOut(400);
-	sliderNum++; checkI();
-	$("#header #container"+sliderNum).delay(400).fadeIn();
-	
-})
+	})
 
-$("#header .fa-chevron-left").on("click",function(){
-	$("#header #container"+sliderNum).fadeOut(400);
-	sliderNum--; checkI();
-	$("#header #container"+sliderNum).delay(400).fadeIn();
-})
+	$("#header .fa-chevron-left").on("click",function(){
+		$("#header #container"+sliderNum).fadeOut(400);
+		sliderNum--; checkI();
+		$("#header #container"+sliderNum).delay(400).fadeIn();
+	})
 
-	// Function to keep sliderNum between 1 and 3
-	function checkI(){
-		if(sliderNum==4){
-			sliderNum=1;
-		}else if(sliderNum==0){
-			sliderNum=3;
+		// Function to keep sliderNum between 1 and 3
+		function checkI(){
+			if(sliderNum==4){
+				sliderNum=1;
+			}else if(sliderNum==0){
+				sliderNum=3;
+			}
 		}
-	}
+	// Floating elements animation
+	$("#header").mousemove(function(){
+		var X=event.pageX;
+		var Y=event.pageY;
+		$("#desktop").css("top",Y/100).css("left",200+X/100);
+		$("#tablet").css("top",20+Y/70).css("left",X/70-130);
+		$("#phone").css("top",30+Y/85).css("left",X/85+60);
+		$("#container1 h1, #container1 p").css("position","relative").css("top",Y/70).css("left",X/70);
+	})
+	
 
 // Offers
 	// Function to make all divs' height same
