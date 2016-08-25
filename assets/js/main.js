61214318ed9435a1b70497d09d016483982b6c48
Chingiz Mammadli <memmmedlicngz@gmail.com>
2016-08-25 21:23:27 +0400

    Added images to port;changed a little bit;added floating element animations to all header containers

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,20 +26,44 @@
 	$("#header").mousemove(function(){
 		var X=event.pageX;
 		var Y=event.pageY;
+		
+		// Container1
 		$("#desktop").css("top",Y/100).css("left",200+X/100);
 		$("#tablet").css("top",20+Y/70).css("left",X/70-130);
 		$("#phone").css("top",30+Y/85).css("left",X/85+60);
 		$("#container1 h1, #container1 p").css("position","relative").css("top",Y/70).css("left",X/70);
+		
+		// Container2
+		$("#skew1").css("top",Y/100-160).css("left",10+X/100);
+		$("#skew2").css("top",Y/70-350).css("left",X/70-220);
+		$("#skew3").css("top",Y/85-445).css("left",40+X/85);
+		$("#container2 h1, #container2 p").css("position","relative").css("top",Y/70).css("left",X/70);
+		
+		// Container3
+		$("#skew1").css("top",Y/100-160).css("left",10+X/100);
+		$("#skew2").css("top",Y/70-350).css("left",X/70-220);
+		$("#skew3").css("top",Y/85-445).css("left",40+X/85);
+		$("#container3 h1, #container3 p").css("position","relative").css("top",Y/70).css("left",X/70);
 	})
 	
 
 // Offers
 	// Function to make all divs' height same
 	var maxHeight=0;
-	for(i=1;i<5;i++){
+	for(i=1;i<=4;i++){
 		thisH=$("#offers .col-md-3:nth-child("+i+")").height();
 		if(thisH>maxHeight){
 			maxHeight=thisH;
 		}
 	}
 	$("#offers .col-md-3 .offer").height(thisH);
+
+// Portfolio
+	// Hover animations
+	$("#portfolio .portItem").mouseenter(function(){
+		$(this).find("img").animate({width:"102%",left:"-1%",top:"-2px"},300);
+	})
+	
+	$("#portfolio .portItem").mouseleave(function(){
+		$(this).find("img").animate({width:"100%",left:"0",top:"0"},300);
+	})
